515c7c028c144d42aef355b431b4dd2e2b0e26c3
ronkeadun <ronkeadun1@yahoo.com>
2019-11-27 05:52:49 +0100

    feature:create gifs comment [Delivers #10]

--- server/controllers/commentsControllers.js
+++ server/controllers/commentsControllers.js
@@ -18,7 +18,6 @@ const Comment = {
 	    			"message": "article not found"
 	    		});
 			}
-			console.log(result.rows[0])
 
 			const values = [
 				req.body.comment,
@@ -44,6 +43,52 @@ const Comment = {
 						"createdOn" : q_res.rows[0].createdon,
 						"articleTitle" : q_res.rows[0].articletitle,
 						"article" : result.rows[0].article,
+						"comment" : q_res.rows[0].comment
+					}
+				})
+			})
+		});	
+	},
+
+	/**
+   * create gif comment
+   * @param {object} req 
+   * @param {object} res
+   * @returns {object} user object 
+   */
+	createGifComment(req,res,next){
+		const findOneQuery = 'SELECT * FROM gifs WHERE gifid=$1 AND user_id = $2';
+		pool.query(findOneQuery, [req.params.gifId, req.userData.userId], (err,result)=>{
+			if(!result.rows[0]) {
+				return res.status(404).json({
+	    			"status" : "error",
+	    			"message": "gif not found"
+	    		});
+			}
+
+			const values = [
+				req.body.comment,
+				result.rows[0].title,
+				req.userData.userEmail,
+				req.userData.userId,
+				req.params.gifId,
+			]
+
+			pool.query(`INSERT INTO comments(comment,gifTitle,author,user_id,gif_id,createdOn)
+			VALUES($1,$2,$3,$4,$5,NOW() ) RETURNING *`,values, 
+			(q_err,q_res)=>{
+				if(q_err){
+					return res.status(400).json({
+			    		"status" : "error",
+			    		"message": q_err.detail
+			    	});		
+				}
+				res.json({
+					"status" : "success",
+					"data" : {
+						"message" : "comment successfully created",
+						"createdOn" : q_res.rows[0].createdon,
+						"gifTitle" : q_res.rows[0].giftitle,
 						"comment" : q_res.rows[0].comment
 					}
 				})
